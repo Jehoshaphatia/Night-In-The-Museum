da94df9c2f063af92e006a03fef3e7ea90e02634
Jehoshaphat I. Abu <loveyou725@hotmail.com>
2017-10-07 15:12:19 +0100

    Added new city scape skybox and city ambient sound

---
+++ Assets/Udacity/Scripts/Kim.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+[RequireComponent(typeof(AudioSource))]
+public class Kim : MonoBehaviour
+{
+
+    public GameObject kkhair;
+    public AudioSource audio;
+
+    void Start()
+    {
+        audio = GetComponent<AudioSource>();
+    }
+
+    public void speakKim()
+    {
+        audio.Play();
+    }
+}
