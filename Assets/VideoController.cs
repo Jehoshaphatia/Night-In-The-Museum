353a0377d02423b2eecb7127df77ce6484aa42f2
Jehoshaphat I. Abu <loveyou725@hotmail.com>
2017-10-07 03:31:14 +0100

    Added ambient sound and feedforward sounds

---
+++ Assets/VideoController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class VideoController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        ((MovieTexture)GetComponent<Renderer>().material.mainTexture).Play();
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
