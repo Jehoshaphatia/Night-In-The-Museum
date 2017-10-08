16233cd6d71799a5af84a74994c97504cff0e08e
Jehoshaphat I. Abu <loveyou725@hotmail.com>
2017-10-08 19:18:32 +0100

    Added Motion

---
+++ Assets/Udacity/Scripts/StartPoint.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartPoint : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject startPoint, playPoint;
+
+    public bool playerWon = false;
+
+    // Use this for initialization
+    void Start()
+    {
+      player.transform.position = startPoint.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void enterGalary()
+    { //Begin the puzzle sequence
+        iTween.MoveTo(player,
+            iTween.Hash(
+                "position", playPoint.transform.position,
+                "time", 2,
+                "easetype", "linear"
+            )
+        );
+
+    }
+
+}
